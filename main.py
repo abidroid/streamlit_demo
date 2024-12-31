d511ee7d2ebd69c4839a30b8f4e39146c13b991d
Muhammad Abid <abidroid1@gmail.com>
2024-12-31 11:17:19 +0500

    streamlit write

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import streamlit as st
 
-st.title("Streamlist Widgets Example")
+# Write and magic
+st.write("Hello **world**!")
+st.write("Hello, *World!* :sunglasses:")
+
+
+
+st.title("Streamlit Widgets Example")
 st.header("Basic Widgets")
+st.subheader("Sub Heading")
+st.caption("Small Text")
+st.code(""" 
+a = 24
+print(a)
+""")
 
-name = st.text_input("What's your name")
-age = st.number_input("What's your age?", min_value=0, max_value=120, value=18)
 
-if st.button("Submit"):
-    st.write(f"Hello, {name}! You are {age} years old")
+
+
+# name = st.text_input("What's your name")
+# age = st.number_input("What's your age?", min_value=0, max_value=120, value=18)
+
+# if st.button("Submit"):
+#     st.write(f"Hello, {name}! You are {age} years old")
