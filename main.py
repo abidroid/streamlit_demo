848a828acb4231b15c050d150eddac496be5c1f9
Muhammad Abid <abidroid1@gmail.com>
2025-01-02 20:54:36 +0500

    numpy basics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 # Write and magic
 st.write("Hello **world**!")
@@ -15,8 +16,23 @@ a = 24
 print(a)
 """)
 
+st.subheader('Numpy Understandings')
+arr_1d = np.array([1, 2, 3, 4, 5]) # **np.array()** is used to create NumPy arrays.
+st.write(arr_1d)
 
+arr_2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+st.write(arr_2d)
+st.write(arr_2d.ndim)
+st.write(arr_2d.shape)
+st.write(arr_2d.size)
 
+# Array addition
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+result = array1 + array2
+st.write(array1)
+st.write( array2)
+st.write(result)
 
 # name = st.text_input("What's your name")
 # age = st.number_input("What's your age?", min_value=0, max_value=120, value=18)
