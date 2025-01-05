c0732cf7f25613360f167f6336210274b96a0ab8
Muhammad Abid <abidroid1@gmail.com>
2025-01-05 21:56:47 +0500

    read all a tags text

--- pages/web_scrapping.py
+++ pages/web_scrapping.py
@@ -14,3 +14,9 @@ html_content = response.text
 soup = BeautifulSoup(html_content, 'html.parser')
 
 st.write(html_content[:500])
+
+# Find all <a> tags (anchor tags) in the HTML
+links = soup.find_all('a')
+# Iterate through the list of links and print their text
+for link in links:
+    st.write(link.text)
